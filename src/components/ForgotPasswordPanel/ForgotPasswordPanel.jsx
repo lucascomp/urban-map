3df0619309816232ffa736c8ffc5402c2a35c117
lucascomp <lucas_comp@outlook.com>
2019-09-17 02:45:23 -0300

    ForgotPasswordPanel component

---
+++ src/components/ForgotPasswordPanel/ForgotPasswordPanel.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ForgotPasswordForm from '../ForgotPasswordForm';
+import Panel from '../Panel';
+import styles from './ForgotPasswordPanel.css';
+
+const ForgotPasswordPanel = () => (
+  <Panel>
+    <h3 className={styles.Title}>Mapa de Acessibilidade Urbana</h3>
+    <p className={styles.Text}>
+      Esqueceu sua senha? Não se preocupe. É só nos dizer seu e-mail que enviaremos um link para
+      você cadastrar uma nova senha.
+    </p>
+    <ForgotPasswordForm />
+  </Panel>
+);
+
+export default ForgotPasswordPanel;
