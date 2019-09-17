caf0f2c8b68b08a5623bff727c8279597cbecc03
lucascomp <lucas_comp@outlook.com>
2019-09-17 02:42:44 -0300

    LoginForm component

---
+++ src/components/LoginForm/LoginForm.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import Link from 'next/link';
+import Button from '../Button';
+import TextField from '../TextField';
+import redirect from '../../utils/router';
+import styles from './LoginForm.css';
+
+const LoginForm = () => {
+  const onSubmit = (event) => {
+    event.preventDefault();
+    redirect('/');
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <TextField
+        id="email"
+        name="email"
+        placeholder="E-mail"
+        className={styles.Email}
+        value=""
+        handleChange={() => {}}
+      />
+      <TextField
+        id="password"
+        name="password"
+        type="password"
+        placeholder="Senha"
+        className={styles.Password}
+        value=""
+        handleChange={() => {}}
+      />
+      <Link href="/forgot-password">
+        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
+        <a className={styles.ForgotPasswordLink}>
+        Esqueceu sua senha?
+        </a>
+      </Link>
+      <Button
+        className={styles.SubmitButton}
+        type="submit"
+        fluid
+      >
+        Entrar
+      </Button>
+    </form>
+  );
+};
+
+export default LoginForm;
---
+++ src/utils/router/router.js
@@ -0,0 +1,12 @@
+import Router from 'next/router';
+
+const redirect = (destination, res, status = 302) => {
+  if (res) {
+    res.writeHead(status, { Location: destination });
+    res.end();
+  } else {
+    Router.push(destination);
+  }
+};
+
+export default redirect;
