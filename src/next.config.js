8ca222ab27660dda171b91846d8b1ae2aeef1b3b
lucascomp <lucas_comp@outlook.com>
2019-09-17 02:35:15 -0300

    Add dependencies and update next.config

--- src/next.config.js
+++ src/next.config.js
@@ -1,4 +1,17 @@
-module.exports = {
+const withImages = require('next-images');
+const withCss = require('@zeit/next-css');
+
+const { NODE_ENV } = process.env;
+const prod = NODE_ENV === 'production';
+
+const nextConfig = {
   dir: './src',
   distDir: '../build',
+  cssModules: true,
+  cssLoaderOptions: {
+    url: false,
+    localIdentName: prod ? 'ums-[hash:base64:5]' : '[name]_[local]-[hash:base64:5]',
+  },
 };
+
+module.exports = withImages(withCss(nextConfig));
