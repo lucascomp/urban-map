12572ec4967cb7008ce7ab559239c1725afb3ae9
lucascomp <lucas_comp@outlook.com>
2019-09-17 02:44:25 -0300

    SignUpPanel component

---
+++ src/components/SignUpPanel/SignUpPanel.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import Divison from '../Divison';
+import FacebookLoginButton from '../FacebookLoginButton';
+import GoogleLoginButton from '../GoogleLoginButton';
+import Panel from '../Panel';
+import SignUpForm from '../SignUpForm';
+import redirect from '../../utils/router';
+import styles from './SignUpPanel.css';
+
+const SignUpPanel = () => {
+  const onLoggedIn = () => {
+    redirect('/');
+  };
+
+  return (
+    <Panel>
+      <h3 className={styles.Title}>Mapa de Acessibilidade Urbana</h3>
+      <div className={styles.SocialLoginWrapper}>
+        <FacebookLoginButton className={styles.FacebookButton} onLoggedIn={onLoggedIn} />
+        <GoogleLoginButton className={styles.GoogleButton} onLoggedIn={onLoggedIn} />
+      </div>
+      <Divison>ou crie usando seu e-mail</Divison>
+      <SignUpForm />
+    </Panel>
+  );
+};
+
+export default SignUpPanel;
