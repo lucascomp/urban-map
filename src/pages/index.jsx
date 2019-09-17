1eec9b2d73be159837327b6b21861804d43af263
lucascomp <lucas_comp@outlook.com>
2019-09-17 02:37:48 -0300

    Update index page

--- src/pages/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Index = () => (
-  <div>
-    <p>Hello Next.js</p>
-  </div>
-);
-
-export default Index;
---
+++ src/pages/index/Index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Head from 'next/head';
+
+const Index = () => (
+  <>
+    <Head>
+      <title>Home | Mapa de Acessibilide Urbana</title>
+    </Head>
+    <div>
+      Hello home!
+    </div>
+  </>
+);
+
+export default Index;
