c0d89edbe2b7a0076093b9aec031b5961a269c34
lucascomp <lucas_comp@outlook.com>
2019-09-26 00:44:40 -0300

    Handling states on ForgotPasswordForm

--- components/ForgotPasswordForm/ForgotPasswordForm.jsx
+++ components/ForgotPasswordForm/ForgotPasswordForm.jsx
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '../Button';
 import TextField from '../TextField';
 import redirect from '../../utils/router';
 import styles from './ForgotPasswordForm.css';
 
 const ForgotPasswordForm = () => {
+  const [email, setEmail] = useState('');
+  const [submitting, setSubmitting] = useState(false);
+
   const onSubmit = (event) => {
     event.preventDefault();
+    setSubmitting(true);
     redirect('/login');
   };
+
+  const onEmailChanged = ({ target: { value } }) => setEmail(value);
 
   return (
     <form onSubmit={onSubmit}>
@@ -17,12 +23,14 @@ const ForgotPasswordForm = () => {
         name="email"
         placeholder="E-mail"
         className={styles.Email}
-        value=""
-        handleChange={() => {}}
+        value={email}
+        handleChange={onEmailChanged}
       />
       <Button
         type="submit"
         fluid
+        disabled={submitting}
+        submitting={submitting}
       >
         Enviar
       </Button>
