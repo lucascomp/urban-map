86e961cecfc4fea7afc828a6697ea13e560f6750
lucascomp <lucas_comp@outlook.com>
2019-10-30 23:44:39 -0300

    Marking local position

--- components/Map/Map.jsx
+++ components/Map/Map.jsx
@@ -3,11 +3,27 @@ import { GoogleMap, LoadScript } from '@react-google-maps/api';
 import MapComponents from '../MapComponents';
 
 /* global navigator */
+/* global google */
 
 const Map = () => {
   const onLoad = (map) => {
+    const localMarker = new google.maps.Marker({
+      map,
+      icon: {
+        path: google.maps.SymbolPath.CIRCLE,
+        scale: 7.5,
+        fillColor: '#f17569',
+        fillOpacity: 1,
+        strokeWeight: 0.2,
+      },
+    });
+
     navigator.geolocation.getCurrentPosition(({ coords: { latitude, longitude } }) => {
       map.panTo({ lat: latitude, lng: longitude });
+    });
+
+    navigator.geolocation.watchPosition(({ coords: { latitude: lat, longitude: lng } }) => {
+      localMarker.setPosition({ lat, lng });
     });
   };
 
@@ -15,7 +31,7 @@ const Map = () => {
     <LoadScript libraries={['places']} googleMapsApiKey={process.env.GOOGLE_MAPS_API_KEY}>
       <GoogleMap
         mapContainerStyle={{ height: '100vh' }}
-        zoom={15}
+        zoom={20}
         center={{
           lat: -22.906955,
           lng: -43.186902,
