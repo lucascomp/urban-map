3737daf2b5b6f84cdc45c2c47e1229978a965fca
lucascomp <lucas_comp@outlook.com>
2020-08-18 00:12:02 -0300

    Fixing public dir

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ const handle = app.getRequestHandler();
   server
     .use(compression())
     .use(cookieParser())
-    .use(express.static('public'));
+    .use(express.static(`${__dirname}/public`));
 
   if (dev) {
     server.get('/_next/*', handle);
