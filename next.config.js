9b2e148cbfe6eca54953a4435929ab96ecc68dae
lucascomp <lucas_comp@outlook.com>
2020-08-18 00:07:47 -0300

    Fixing dependencies

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 
 const withImages = require('next-images');
 const withCss = require('@zeit/next-css');
-const withOffline = require('next-offline');
 const path = require('path');
 const Dotenv = require('dotenv-webpack');
 const { version } = require('./package.json');
@@ -33,29 +32,6 @@ const nextConfig = {
     url: false,
     localIdentName: prod ? 'ums-[hash:base64:5]' : '[name]_[local]-[hash:base64:5]',
   },
-  target: 'serverless',
-  transformManifest: (manifest) => ['/'].concat(manifest),
-  generateInDevMode: true,
-  // workboxOpts: {
-  //   swDest: 'static/service-worker.js',
-  //   runtimeCaching: [
-  //     {
-  //       urlPattern: /^https?.*/,
-  //       handler: 'NetworkFirst',
-  //       options: {
-  //         cacheName: 'https-calls',
-  //         networkTimeoutSeconds: 15,
-  //         expiration: {
-  //           maxEntries: 150,
-  //           maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
-  //         },
-  //         cacheableResponse: {
-  //           statuses: [0, 200],
-  //         },
-  //       },
-  //     },
-  //   ],
-  // },
 };
 
-module.exports = withOffline(withImages(withCss(nextConfig)));
+module.exports = withImages(withCss(nextConfig));
